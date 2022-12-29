4c0d0e171742902035f22300625ef49d7f51cfe7
David Pratten <david@prattenmail.com>
2022-12-30 07:20:34 +1100

    reformat idr_query.py

--- jetisu/idr_query.py
+++ jetisu/idr_query.py
@@ -3,22 +3,23 @@
 
 # Basic imports and setup
 
-# In[535]:
-from sqlglot.executor import execute
-from sqlglot import exp, parse_one
-import subprocess
 import json
 import os
-import tempfile
-import sys
 import re
+import subprocess
+import sys
+import tempfile
+
+from sqlglot import exp, parse_one
+from sqlglot.executor import execute
+
 
 def idr_read_model(canonical_table_name):
     with open('jetisu/' + canonical_table_name + '.mzn', 'r') as file:
         return file.read()
 
-def idr_query(SQL, return_data):
 
+def idr_query(sql_query, return_data):
     assert return_data in ['data', 'markdown table', 'model', 'constrained model']
 
     # handles a conjuction of simple constraint clauses only TODO Generalise this code.
@@ -27,17 +28,14 @@ def idr_query(SQL, return_data):
     column_name = ''
     found_where = False
     found_constraints = False
-    found_EQ = False
-    found_Not = False
-    found_Negated_Boolean = False
+    found_eq = False
+    found_not = False
     too_complex = False
     eq_constraints = {}
 
-    for node_tuple in parse_one(SQL).walk(bfs=False):
-        # print(type(node_tuple[0]), node_tuple[0].name)
+    for node_tuple in parse_one(sql_query).walk(bfs=False):
         if isinstance(node_tuple[0], exp.Identifier):
             # for current purposes the Identifiers may be ignored.
-            # print('ignoring', str(node_tuple[0]))
             continue
         if isinstance(node_tuple[0], exp.Group) or isinstance(node_tuple[0], exp.Having) or isinstance(node_tuple[0],
                                                                                                        exp.Order):
@@ -61,106 +59,69 @@ def idr_query(SQL, return_data):
             else:
                 too_complex = True
                 continue
-        if found_constraints and not found_EQ and not found_Not:
+        if found_constraints and not found_eq and not found_not:
             if isinstance(node_tuple[0], exp.EQ):
-                found_EQ = True
+                found_eq = True
                 continue
             if isinstance(node_tuple[0], exp.Not):
-                found_Not = True
+                found_not = True
                 continue
             if isinstance(node_tuple[0], exp.Column):
                 eq_constraints[str(node_tuple[0]).lower()] = True
                 continue
             too_complex = True
             continue
-        if found_constraints and found_EQ and column_name == '':
+        if found_constraints and found_eq and column_name == '':
             if isinstance(node_tuple[0], exp.Column):
                 column_name = str(node_tuple[0]).lower()
                 continue
             too_complex = True
             continue
-        if found_constraints and found_EQ and column_name != '':
+        if found_constraints and found_eq and column_name != '':
             if isinstance(node_tuple[0], exp.Literal):
                 # print(node_tuple[0].is_string, str(node_tuple[0]))
 
                 eq_constraints[column_name] = str(node_tuple[0]) if node_tuple[0].is_string else float(
                     str(node_tuple[0]))
-                found_EQ = False
+                found_eq = False
                 column_name = ''
                 continue
             too_complex = True
             continue
-        if found_constraints and found_Not:
+        if found_constraints and found_not:
             if isinstance(node_tuple[0], exp.Column):
                 eq_constraints[str(node_tuple[0]).lower()] = False
-                found_Not = False
+                found_not = False
                 continue
             too_complex = True
             continue
 
-    # print('table', table_name)
-    # print(where_clause)
-    # print(eq_constraints)
-    # print(too_complex)
-
-    # In[538]:
     canonical_table_name = table_name.lower()
     model = idr_read_model(canonical_table_name)
 
-    # print(model)
-
     # Merge in the constraints in the query to get the model to be fed to MiniZinc
 
-    parameters = re.search("predicate +" + canonical_table_name + " *\(([^\)]+?)\)", model, re.S)[1].lower()
+    parameters = re.search(r"predicate +" + canonical_table_name + r" *\(([^)]+?)\)", model, re.S)[1].lower()
     variables = parameters.replace(",", ";") + ";"
     primary_constraint = 'constraint ' + canonical_table_name + '(' + ', '.join(
         [x.split(":")[1] for x in parameters.split(",")]) + ');'
 
-    # print(variables)
-    # print(primary_constraint)
-
     where_clause_data = '; '.join([(x + "= true" if len(x.split("=")) == 1 else x) for x in
                                    where_clause.lower().replace("where", "").split("and")])
-    # print(where_clause_list)
     constrained_model = model + "\n" + variables + "\n" + primary_constraint + "\n" + where_clause_data + ";"
-    # print(model)
     model_fn = tempfile.NamedTemporaryFile().name
     mf = open(model_fn + ".mzn", 'w')
     mf.write(constrained_model)
     mf.close()
 
-    # print (model)
-
     # Run MiniZinc model and save result
-
-    # In[541]:
 
     path_to_minizinc = "C:/Program Files/MiniZinc/minizinc" if sys.platform.startswith('win32') else "/usr/bin/minizinc"
 
-    # path_to_optimathsat = "C:/Program Files/MiniZinc/bin/optimathsat" if sys.platform.startswith(
-    #     'win32') else "/usr/bin/optimathsat"
-    # print(constrained_model)
     result = subprocess.run(
         [path_to_minizinc, "--solver", "optimathsat", model_fn + ".mzn"],
         stdout=subprocess.PIPE)
     output = result.stdout.decode('utf-8')
-    # print('here', output)
-    #sys.exit(0)
-    # In[542]:
-
-    # result = subprocess.run(["C:\Program Files\MiniZinc\minizinc.exe", "-c", model_fn], stdout=subprocess.PIPE)
-    # print(result.stdout.decode('utf-8'))
-
-    # result = subprocess.run(
-    #     [path_to_optimathsat, "-input=fzn", "-opt.fzn.max_solutions=1000",
-    #      "-opt.fzn.finite_precision=12", "-opt.fzn.finite_precision_model=true",
-    #      "-opt.fzn.all_solutions=true", "-opt.fzn.output_var_file=-", "-model_generation=true",
-    #      model_fn + ".fzn"],
-    #     stdout=subprocess.PIPE)  # "-opt.fzn.output_var_file="+model_fn+"_vars.txt",
-    # output = result.stdout.decode('utf-8')
-    # print(output)
-
-    # In[543]:
 
     data_output = []
     data = False
@@ -183,46 +144,20 @@ def idr_query(SQL, return_data):
     solver_data = json.loads('[' + ', '.join(data_output) + ']')
     # print(solver_data)
 
-    # In[544]:
-
-    # clean up TODO Complete the cleanup
     os.remove(model_fn + ".mzn")
-    # os.remove(model_fn + ".ozn")
-    # os.remove(model_fn + ".fzn")
 
     # Put output into a list of dict as data ready to run
 
-    # In[545]:
-
     table_input = {table_name.lower(): [x | eq_constraints for x in solver_data]}  # | eq_constraints
-    # print(eq_constraints)
-    # print(table_input)
 
     # Run the query
-
-    # In[546]:
-
-    # print (SQL)
     res = execute(
-        SQL,
+        sql_query,
         tables=table_input
     )
 
-    # In[547]:
-
-    # short_input = {'act_conveyance_duty': table_input['act_conveyance_duty'][:4]}
-    # SQLtest = """select
-    #     product_name,
-    #     min(price) min_price
-    # from products
-    # group by product_name
-    #     ;"""
-    # res = execute(
-    #     """select product_name, imported,  min(price) min_price from products group by product_name, imported;""",
-    #     tables={'products':[{'product_name': 'Bike', 'price': 100, 'imported': True},{'product_name': 'Tent', 'price': 200, 'imported': True},{'product_name': 'Bike', 'price': 300,  'imported': False}]}
-    # )
     if return_data == 'data':
-        return (res.columns, res.rows)
+        return res.columns, res.rows
     elif return_data == 'markdown table':
         return '|' + '|'.join(res.columns) + '|' + "\n" + '|' + '|'.join(
             ["----" for x in res.columns]) + '|' + "\n" + "\n".join(
@@ -232,15 +167,5 @@ def idr_query(SQL, return_data):
     elif return_data == 'constrained model':
         return constrained_model
     else:
-        return 'Programming error this should never occur'
-    # print(res.columns)
-    # print()
+        return 'Programming error - this should never occur'
 
-    # print(table_mkdn)
-    # print(res.columns)
-    # for i in res.rows:
-    #     print(i)
-    #     for j in i:
-    #         print (j)
-
-    # Present the output.
